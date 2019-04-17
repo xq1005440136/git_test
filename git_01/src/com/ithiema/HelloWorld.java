813b7a819913d037d420fda1c8f6237333713355
admin <xiaoqiang@163.com>
2019-04-17 19:25:13 +0800

    把idea的文件添加到git

---
+++ git_01/src/com/ithiema/HelloWorld.java
@@ -0,0 +1,38 @@
+package com.ithiema;
+
+import org.junit.Test;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+    }
+    @Test
+    public void test(){
+        System.out.println("xiaoqiang");
+
+
+
+    }
+    @Test
+    public void test2(){
+
+
+        System.out.println("hahh");
+    }
+}
